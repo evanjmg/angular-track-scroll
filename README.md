630f92bac7fa85dcf07c7f69a93a5993b648afc9
Evan Gillogley <evanjmg@gmail.com>
2016-02-28 10:03:46 +0000

    made quick demo

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 A simple scroll directive to track and change ng-repeat index/id on scroll
 
 #Setup
+
 #Usage
+
+#To Dos
+- Setup Grunt serve
+- Extract and rewrite scrollTo animation method
+- Add $window option
+- Add Tests
---
+++ dist/angular-track-scroll.js
@@ -0,0 +1,44 @@
+(function (){
+		'use strict';
+
+		angular
+			.module('trackScroll', ['duScroll'])
+
+			.directive('trackScroll', trackScroll);
+
+			trackScroll.$inject = ['$timeout'];
+
+		function trackScroll ($timeout){
+			return {
+				'restrict': 'A',
+				'scope': { 'trackingId': '=',
+										'trackHeight': '=', 
+										'trackDuration': '=' },
+				 link: function (scope, element) { 
+				 	var otherEvent = false;
+						angular.element(element).bind("scroll", function() {
+								if (element[0].scrollTop && !otherEvent) {
+										scope.trackingId = parseInt((element[0].scrollTop)/scope.trackHeight); 
+										scope.$digest();
+								}
+							
+       	 		});
+						scope.$watch(function () {
+							return scope.trackingId;
+						}, function () {
+								
+								var amount = scope.trackHeight * scope.trackingId;
+									otherEvent = true;
+							element.scrollTop(amount, scope.trackDuration);
+							$timeout(function() {
+								otherEvent = false;
+							}, scope.trackDuration);	 
+						});
+      
+         scope.$on('destroy', function (){
+                angular.element($window).unbind('scroll');
+         });
+				 }
+			}
+		}
+	})();
